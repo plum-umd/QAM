e378d42205a103f5fe743384bae6b8e59f095939
lec9243 <lec9243@gmail.com>
2023-01-29 21:01:14 -0500

    update syntax

--- QAMDef.v
+++ QAMDef.v
@@ -6,23 +6,29 @@ Definition prob := nat.
 Definition message := nat.
 Definition channel := nat.
 Definition repeater := nat.
+Definition tau := nat. (* time *)
 
 Inductive action : Type :=
-| Tnil : action
-| TSend : prob -> channel -> message -> action
-| TRecv : channel -> message -> action.
+| TSend (p: prob) (c: channel) (m: message)
+| TRecv (c: channel) (x: var).
                                 
-
 Inductive process : Type :=
-| TNil : process
-| TAct : action -> process -> process
-| TParal : process -> process -> process.
+| TNil
+| TAct (a: action) (p: process)
+| TParal (p1: process) (p2: process).
 
 Definition relation :=  list (repeater * repeater * prob).
-Definition relation_update := nat -> relation. 
+Definition relation_update := tau -> relation -> relation.  
+
+Inductive PCell : Type :=
+|SingleP (p: process) (n: nat) (c: channel)
+|MultiP (p: process) (n: nat) (c: channel) (plst: PCell).  
 
 Inductive config : Type :=
-| Config : list (process * repeater * nat) -> relation -> relation_update -> config.
-.
+| PRConfig : PCell -> relation -> config
+| Config : PCell -> relation -> relation_update -> config.
+
+Notation "p1 | p2" := (TParal p1 p2) (at level 50).
+
 
 
